a620035147c823d0c04399e72d38ca9a9b6d7ebe
Mirko Nasato <mirko@encoded.io>
2016-10-23 14:07:18 +0200

    keep scss files similar to ionic2-app-base

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import './theme/variables.scss';
 import './app/app.scss';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
