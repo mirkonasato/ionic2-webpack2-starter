d131ffe198ac08293f27a2eff095f4ade03a8acd
Mirko Nasato <mirko@encoded.io>
2016-12-14 17:02:35 +0100

    wait for cordova to be ready before bootstrapping the angular app

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,17 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
+function bootstrap() {
+  platformBrowserDynamic().bootstrapModule(AppModule);
+}
+
 console.info('app.environment:', app.environment);
 if (app.environment === 'production') {
   enableProdMode();
 }
-platformBrowserDynamic().bootstrapModule(AppModule);
+
+if (window['cordova']) {
+  document.addEventListener('deviceready', () => bootstrap());
+} else {
+  bootstrap();
+}
